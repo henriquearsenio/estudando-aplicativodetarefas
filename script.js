ee695288b7d3470cf4749d6ac754c73092834265
Henrique Arsenio <105521096+henriquearsenio@users.noreply.github.com>
2023-08-15 12:51:09 -0300

    funcao adicionar tarefa e botao remover js

---
+++ script.js
@@ -0,0 +1,23 @@
+const novaTarefa = document.getElementById("novaTarefa");
+const adicionarBotao = document.getElementById("adicionar");
+const listaTarefas = document.getElementById("listaTarefas");
+
+adicionarBotao.addEventListener("click", function () {
+    const tarefaTexto = novaTarefa.trim();
+    if (tarefaTexto !== "") {
+        const tarefaLi = document.createElement("li");
+        tarefaLi.innerHTML = `
+            <input type="checkbox">
+            <span>${tarefaTexto}</span>
+            <button class="remover">Remover</button>
+        `;
+        listaTarefas.appendChild(tarefaLi);
+        novaTarefa.value = "";
+    }
+});
+
+listaTarefas.addEventListener("click", function (evento) {
+    if (evento.target.classList.contains("remover")) {
+        evento.target.parentElement.remove();        
+    }    
+})
